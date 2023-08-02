06863a818e3560d06f383ea2f96b911fc7527382
Anitha Venkatesan <140714875+1-anitha@users.noreply.github.com>
2023-08-02 15:51:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDoL from './component/dolist'
+
+
+
+function App() {
+  return (
+    <div id='myproject'>
+      <div id='body'>
+        <ToDoL />
+      </div>
+    </div>
+  );
+};
+
+export default App;
